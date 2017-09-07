cda935e2c6951c5ebb17b8188f7b312f1fb03204
Remo <ik.ben.remo@gmail.com>
2017-09-07 05:02:38 +0200

    Work on JS stuff.

--- app/index.js
+++ app/index.js
@@ -1,25 +1,54 @@
 import './main.css'
 
 function clearAutoCompleteItems () {
-  const elements = document.querySelectorAll('.auto-complete__item')
+  const elements = document.querySelectorAll('.autocomplete-item')
   for(let i = 0; i < elements.length; i++) {
     elements[i].remove()
   }
 }
 
-function test (e) {
-  clearAutoCompleteItems()
-  fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${e.currentTarget.value}&limit=10`)
+function getCompleteItems (query) {
+  fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${query}&limit=20&titles=File:Test.jpg&prop=imageinfo`)
     .then(resp => resp.json())
-    .then(data => data[1].forEach( x => {
+    .then(data => data[1].forEach( (x, i) => {
+      console.log(data)
       let el = document.createElement('div')
-      el.setAttribute('class', 'auto-complete__item')
-      el.innerHTML = x
-      document.getElementById('auto-complete').append(el);
+      el.setAttribute('class', 'autocomplete-item')
+      const html = `
+        <a class="autocomplete-item__link" href="${data[3][i]}">
+          <h2 class="autocomplete-item__header">${x}</h2>
+          <p class="autocomplete-item__desc">${data[2][i]}</p>
+        </a>`
+
+      el.innerHTML = html
+      document.getElementById('autocomplete').append(el);
+      el.onclick = (e) => {
+        document.getElementById('input_form').value = x
+        clearAutoCompleteItems()
+      }
     })
   )
 }
 
-document.getElementById('input_form').addEventListener("keyup", test);
+function autocomplete (e) {
+  const query = e.currentTarget.value;
+  clearAutoCompleteItems()
+  if ( e.key !== "Enter" ) {
+    if ( query !== '') {
+      getCompleteItems(query)
+    }
+  } else {
+    if ( query !== "" || query !== " ") {
+      window.open(`https://en.wikipedia.org/wiki/${query}`)
+    }
+  }
+}
 
+document.getElementById('input_form').addEventListener("keyup", autocomplete);
+document.getElementById('input_form').onfocus = () => {
+  if (document.getElementById('input_form').value === 'search Wikipedia…') {
+    document.getElementById('input_form').value = ''
+  }
+}
+document.getElementById('input_form').value = 'search Wikipedia…'
 
