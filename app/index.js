df34e28e7a436b2ba3166cecb5d1514bd127ca8c
Remo <ik.ben.remo@gmail.com>
2017-09-08 04:51:30 +0200

    Clean up the script. Move the logic around.

--- app/index.js
+++ app/index.js
@@ -1,54 +1,77 @@
 import './main.css'
 
+const inputForm = document.getElementById('input_form')
+const letsGoArrow = document.getElementById('lets-go')
+const inputContainer = document.querySelector('.input-form__container')
+const headerElm = document.querySelector('header')
+
 function clearAutoCompleteItems () {
   const elements = document.querySelectorAll('.autocomplete-item')
-  for(let i = 0; i < elements.length; i++) {
+  for (let i = 0; i < elements.length; i++) {
     elements[i].remove()
+  }
+}
+
+function goToWikipedia (e) {
+  if (e.key === 'Enter' && e.currentTarget.value !== '') {
+    window.open('https://en.wikipedia.org/wiki/' + e.currentTarget.value, '_self')
   }
 }
 
 function getCompleteItems (query) {
   fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${query}&limit=20&titles=File:Test.jpg&prop=imageinfo`)
     .then(resp => resp.json())
-    .then(data => data[1].forEach( (x, i) => {
-      console.log(data)
+    .then(data => data[1].forEach((x, i) => {
       let el = document.createElement('div')
       el.setAttribute('class', 'autocomplete-item')
+      el.setAttribute('data-word', x)
       const html = `
-        <a class="autocomplete-item__link" href="${data[3][i]}">
+        <div class="autocomplete-item__container">
           <h2 class="autocomplete-item__header">${x}</h2>
           <p class="autocomplete-item__desc">${data[2][i]}</p>
-        </a>`
+        </div>
+        <a class="autocomplete-item__shortcut" href="${data[3][i]}">Go to the Wikipage»</a>`
 
       el.innerHTML = html
-      document.getElementById('autocomplete').append(el);
-      el.onclick = (e) => {
-        document.getElementById('input_form').value = x
+      document.getElementById('autocomplete').append(el)
+      el.firstElementChild.onclick = e => {
+        e.preventDefault()
+        var textInput = document.getElementById('input_form')
+        headerElm.scrollTop = 0
+        textInput.value = x
+        textInput.focus()
+        textInput.setSelectionRange(textInput.value.length, textInput.value.length)
+
         clearAutoCompleteItems()
+        getCompleteItems(x)
       }
     })
   )
 }
 
-function autocomplete (e) {
-  const query = e.currentTarget.value;
+function fetchResults (e) {
+  const query = e.currentTarget.value
   clearAutoCompleteItems()
-  if ( e.key !== "Enter" ) {
-    if ( query !== '') {
-      getCompleteItems(query)
-    }
+  if (query !== '') {
+    getCompleteItems(query)
+  }
+  if (query === '' || query === 'search Wikipedia…') {
+    letsGoArrow.style.visibility = 'hidden'
   } else {
-    if ( query !== "" || query !== " ") {
-      window.open(`https://en.wikipedia.org/wiki/${query}`)
-    }
+    letsGoArrow.style.visibility = 'visible'
   }
 }
-
-document.getElementById('input_form').addEventListener("keyup", autocomplete);
-document.getElementById('input_form').onfocus = () => {
-  if (document.getElementById('input_form').value === 'search Wikipedia…') {
-    document.getElementById('input_form').value = ''
+letsGoArrow.onclick = e => {
+  window.open('https://en.wikipedia.org/wiki/' + inputForm.value, '_self')
+}
+inputForm.oninput = fetchResults
+inputForm.onkeydown = goToWikipedia
+inputForm.onfocus = () => {
+  inputContainer.classList.add('isFocused')
+  if (inputForm.value === 'search Wikipedia…') {
+    inputForm.value = ''
   }
 }
-document.getElementById('input_form').value = 'search Wikipedia…'
-
+inputForm.onblur = e => inputContainer.classList.remove('isFocused')
+inputForm.value = 'search Wikipedia…'
+letsGoArrow.style.visibility = 'hidden'
